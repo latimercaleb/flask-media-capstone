6c569c024f1854f50a241f64207fabecf043674e
Exia <nohitokaze@gmail.com>
2026-03-23 02:16:02 -0400

    Adding project setup II

--- app.py
+++ app.py
@@ -1,1 +1,3 @@
-print("Test")
+from blog import app
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ blog/__init__.py
@@ -0,0 +1,5 @@
+from flask import Flask
+app = Flask(__name__)
+# Note setup vm
+# Grab requirements.txt 
+# Grab env file setup and add to gitignore
