d260fe9f3bc6447d1d650eb3d08c6da949a2d741
Exia <nohitokaze@gmail.com>
2026-03-26 19:00:32 -0400

    Adding some setup

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from core.views import core
 app = Flask(__name__)
+app.register_blueprint(core)
 # Note setup vm
 # Grab requirements.txt 
 # Grab env file setup and add to gitignore
