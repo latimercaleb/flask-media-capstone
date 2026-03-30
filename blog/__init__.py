5a6e98ce0d1734826169d532d514302cfdd1b6ea
Exia <nohitokaze@gmail.com>
2026-03-30 01:22:02 -0400

    Adding some logic for error handling

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from blog.core.views import core
+from blog.errors.handlers import errors
 app = Flask(__name__)
 app.register_blueprint(core)
+app.register_blueprint(errors)
 # Note setup vm (DONE)
 # Grab requirements.txt  (DONE)
 # Grab env file setup and add to gitignore (DONE)
---
+++ blog/errors/handlers.py
@@ -0,0 +1,11 @@
+# Error handling 
+from flask import render_template, Blueprint
+errors = Blueprint('errors', __name__)
+
+@errors.app_errorhandler(404)
+def not_found_error(error):
+    return render_template('errors/404.html'), 404
+
+@errors.app_errorhandler(500)
+def internal_error(error):
+    return render_template('errors/500.html'), 500
