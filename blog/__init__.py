9382d513ea0109d458f2d212046f85a06e625cad
Exia <nohitokaze@gmail.com>
2026-03-30 00:23:28 -0400

    Cleaning up templates and reworking import logic, adding nav skeleton

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from blog.core.views import core
 app = Flask(__name__)
 app.register_blueprint(core)
-# Note setup vm
-# Grab requirements.txt 
-# Grab env file setup and add to gitignore
-# Test initial route setup for core cmp path
+# Note setup vm (DONE)
+# Grab requirements.txt  (DONE)
+# Grab env file setup and add to gitignore (DONE)
+# Test initial route setup for core cmp path (DONE)
